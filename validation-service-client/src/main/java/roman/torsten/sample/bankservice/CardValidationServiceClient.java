7d5a63479301de04480adf9024c197eac64ce95b
Roman Torsten <roman.torsten@db.com>
2017-04-19 14:51:39 +0300

    Add SSL for validation service

--- validation-service-client/src/main/java/roman/torsten/sample/bankservice/CardValidationServiceClient.java
+++ validation-service-client/src/main/java/roman/torsten/sample/bankservice/CardValidationServiceClient.java
@@ -1,6 +1,15 @@
 package roman.torsten.sample.bankservice;
 
+import lombok.SneakyThrows;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +17,44 @@ import roman.torsten.sample.bankservice.api.Card;
 import roman.torsten.sample.bankservice.api.CardValidationResult;
 import roman.torsten.sample.bankservice.api.service.CardValidationService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+
 @Service
 public class CardValidationServiceClient implements CardValidationService {
 
+    private static final String keyStoreFile = "validation-service/keystore.p12";
+    private static final String keyStorePassword = "mypassword";
+
+    private static final ClientHttpRequestFactory requestFactory;
+
+    static {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(new FileInputStream(new File(keyStoreFile)), keyStorePassword.toCharArray());
+
+            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+                    new SSLContextBuilder()
+                            .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                            .loadKeyMaterial(keyStore, keyStorePassword.toCharArray())
+                            .build(),
+                    NoopHostnameVerifier.INSTANCE);
+
+            HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+
+            requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
+    @SneakyThrows
     public CardValidationResult validate(Card card) {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        ResponseEntity<CardValidationResult> response = restTemplate.postForEntity("http://localhost:8082/validate", card, CardValidationResult.class);
+        ResponseEntity<CardValidationResult> response = restTemplate.postForEntity("https://localhost:8082/validate", card, CardValidationResult.class);
         return response.getBody();
     }
 
