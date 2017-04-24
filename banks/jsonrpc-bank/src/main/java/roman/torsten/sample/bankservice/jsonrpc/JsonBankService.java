db9f0073ff70251614e4c27bc0feb96e73bb11d4
Roman Torsten <roman.torsten@db.com>
2017-04-24 19:01:55 +0300

    Add GRPC interface and bank stub implementation. Refactoring.

--- banks/jsonrpc-bank/src/main/java/roman/torsten/sample/bankservice/jsonrpc/JsonBankService.java
+++ banks/jsonrpc-bank/src/main/java/roman/torsten/sample/bankservice/jsonrpc/JsonBankService.java
@@ -1,16 +1,18 @@
 package roman.torsten.sample.bankservice.jsonrpc;
 
+import com.googlecode.jsonrpc4j.JsonRpcParam;
+import com.googlecode.jsonrpc4j.JsonRpcService;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 import org.springframework.stereotype.Service;
 import roman.torsten.sample.bankservice.api.TransferRequest;
 import roman.torsten.sample.bankservice.api.TransferResult;
 import roman.torsten.sample.bankservice.api.TransferStatus;
 
+@JsonRpcService("/jsonBankService")
 @AutoJsonRpcServiceImpl
 @Service
-public class JsonBankService implements JsonRpcBankService {
-    @Override
-    public TransferResult transfer(TransferRequest request) {
+public class JsonBankService {
+    public TransferResult transfer(@JsonRpcParam(value = "request") TransferRequest request) {
         System.out.println("Send response to " + request);
         return new TransferResult(TransferStatus.Success, "json ok!");
     }
